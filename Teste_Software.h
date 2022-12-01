ee21387be778487ce7f9de7365aa44fdec2df0a4
gabrieldesouzaf <47822415+gabrieldesouzaf@users.noreply.github.com>
2022-12-01 04:24:42 -0300

    Ajustes funções de teste

---
+++ Teste_Software.h
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#define REP_MAX 2
+
+
+
+class Teste{
+
+private:
+
+int cont_repeticao;
+int cont_entrada;
+
+public:
+
+	Teste();
+	~Teste();
+ int entradas_Ordem();
+ int entradas_Aleatorio();
+	
+
+};
