5909186345bcd6274a4e90186a2e84f7b27f36a1
MarceloSanC <94231979+MarceloSanC@users.noreply.github.com>
2022-12-08 16:21:25 -0300

    Add files via upload

---
+++ interrupt_test.ino
@@ -0,0 +1,77 @@
+#include "MotorDriver.h"
+
+// ========================================================================================================
+// --- Mapeamento de Hardware ---
+#define   vcc      5
+#define   gnd      4
+#define   pino_D0  3
+#define   analog   2
+
+ 
+// ========================================================================================================
+// --- Variáveis Globais --- 
+int           rpm;
+volatile byte pulsos;
+unsigned long timeold, currentTime;
+
+//Altere o numero abaixo de acordo com o seu disco encoder
+unsigned int pulsos_por_volta = 20;
+
+
+// ========================================================================================================
+// --- Interrupção ---
+void contador()
+{
+  //Incrementa contador
+  timeold = currentTime;
+  currentTime = millis();
+  Serial.print(currentTime-timeold);
+
+}
+
+
+// ========================================================================================================
+// --- Configurações Iniciais ---
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(vcc,    OUTPUT);
+  pinMode(gnd,    OUTPUT);
+  pinMode(pino_D0, INPUT);
+  pinMode(analog,  INPUT);
+
+  digitalWrite(vcc, HIGH);
+  digitalWrite(gnd,  LOW);
+  
+  //Interrupcao 1 - pino digital 3
+  //Aciona o contador a cada pulso
+  attachInterrupt(2, contador, FALLING);
+  pulsos = 0;
+  rpm = 0;
+  timeold = 0;
+
+  Serial.println("----NEW----");
+
+  motordriver.init();
+  motordriver.setSpeed(100,MOTORA);
+  motordriver.setSpeed(100,MOTORB);
+
+  motordriver.goForward();
+  
+} //end setup
+
+
+// ========================================================================================================
+// --- Loop Infinito ---
+void loop()
+{ 
+  Serial.println(currentTime);
+  Serial.println(timeold);
+  Serial.println(currentTime-timeold);
+  delay(100);
+} //end loop
+
+
+
+
